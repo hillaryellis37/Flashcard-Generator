290bb8d452165cd73070e10fc546f92312da84fa
Live Sound Systems <hillaryellis37@gmail.com>
2017-10-04 21:35:14 -0700

    added working prototypes

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,34 +1,44 @@
-var BasicCard = require("../BasicCard.js");
+var BasicCard = require("./BasicCard.js");
 
 
-var ClozeCard = function(text, cloze){
+function ClozeCard(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
 };
 
+ClozeCard.prototype.partial = function() {
+	var brokenCloze = "This does not work: " + this.cloze;
+	
+	if (this.text.includes(this.cloze)) {
+		var clozed = this.text.replace(this.cloze, "...");
+		console.log(clozed);
+	} else {
+		console.log(brokenCloze);
+	}
+};
+
+ClozeCard.prototype.fullText = function() {
+		console.log(this.text);
+};		
+
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "Dave");
 
 // "Who was the first president of the United States?"
 console.log(firstPresident.front); 
-
-// "George Washington"
 console.log(firstPresident.back); 
+firstPresidentCloze.partial();
+firstPresidentCloze.fullText();
 
-var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
+// // " ... was the first president of the United States.
+// console.log(firstPresidentCloze.partial); 
 
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
+// // "George Washington was the first president of the United States.
+// console.log(firstPresidentCloze.fullText); 
 
-// " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); "
-
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText): "
-
-// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
-```
+// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+// var brokenCloze = new ClozeCard("This doesn't work", "oops");
 
 module.exports = ClozeCard;
